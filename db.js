74de34e30d14ea9b02fdd38cdf170075ef507094
iamawez <awezmanyar1672@gmail.com>
2025-08-06 22:17:24 +0530

    new update

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const util = require('util');
 dotenv.config();
 
 const pool = mysql.createPool({
-  connectionLimit: 10,
+  connectionLimit: 10, 
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
