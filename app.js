fa89d6af772532b9a49b0c79239d3a243f4f9127
iamawez <awezmanyar1672@gmail.com>
2025-08-16 22:51:37 +0530

    new

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const cors = require('cors');
-
+ 
 dotenv.config();
 const port = process.env.PORT || 5000;
-
+ 
 app.use(express.json());
 app.use(cors());
 
