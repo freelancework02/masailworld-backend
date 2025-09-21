6347482725606b56840a27223edfbdba69e0a85c
iamawez <awezmanyar1672@gmail.com>
2025-09-22 00:10:05 +0530

    first commit

--- routes/fatwaRoutes.js
+++ routes/fatwaRoutes.js
@@ -6,17 +6,16 @@ const fatwaController = require("../controllers/fatwaController");
 router.post("/website", fatwaController.addQuestionFromWebsite); // user submits question
 router.post("/dashboard", fatwaController.addFatwaFromDashboard); // admin creates directly
 
+// Search
+router.get("/search", fatwaController.searchFatawa);  // Search Fatawa by Title, slug, or detailquestion
+
 // Read
 router.get("/", fatwaController.getAllFatwas); // with ?limit=10&offset=0
-// Latest 3 fatawa
-router.get("/latest", fatwaController.getLatestFatwas);
+router.get("/latest", fatwaController.getLatestFatwas); // latest 3 fatawa
 router.get("/:id", fatwaController.getFatwaById);
 
 // Update
 router.put("/:id", fatwaController.updateFatwa);
-
-
-
 
 // Delete (soft delete)
 router.delete("/:id", fatwaController.deleteFatwa);
